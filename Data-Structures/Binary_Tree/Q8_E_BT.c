b9157145ead9c016aef59123da4321fe8ca7ffd6
ThisTimeNull <krsy0411@naver.com>
2025-08-09 20:10:03 +0900

    Q8_E_BT

--- Data-Structures/Binary_Tree/Q8_E_BT.c
+++ Data-Structures/Binary_Tree/Q8_E_BT.c
@@ -79,6 +79,7 @@ int main()
             case 2:
                 printf("\nThe values stored in all nodes of the tree that has at least one great-grandchild are: ");
                 hasGreatGrandchild(root);
+                printf("\n");
                 removeAll(&root);
                 break;
             case 0:
@@ -102,7 +103,18 @@ int main()
 
 int hasGreatGrandchild(BTNode *node)
 {
-	/* add your code here */
+	// 기저 조건
+    if (node == NULL) return 0;
+
+    int leftDepth = hasGreatGrandchild(node->left) + 1;
+    int rightDepth = hasGreatGrandchild(node->right) + 1;
+
+    if (leftDepth > 3 || rightDepth > 3)
+    {
+        printf("%d ", node->item);
+    }
+
+    return (leftDepth > rightDepth) ? leftDepth : rightDepth;
 }
 
 //////////////////////////////////////////////////////////////////////////////////
